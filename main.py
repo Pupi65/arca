301c3370ff367b616258fcd718912776828ffa9b
Pupi65 <100903523+Pupi65@users.noreply.github.com>
2022-04-05 03:53:50 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'Jesiqi'
+        tl_admin_user = 'nautaii'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -540,9 +540,9 @@ def onmessage(update,bot:ObigramClient):
             #    bot_token = os.environ.get('bot_token')
             #    
                 # set in debug
-            #    api_id = 14681595
-            #    api_hash = 'a86730aab5c59953c424abb4396d32d5'
-            #    bot_token = '5289637475:AAFdCLoE7ywnesB31CJLZAafyrb9otgktQM'
+            #    api_id = 7386053
+            #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
+            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
@@ -558,7 +558,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5215345700:AAEN3jml2PV1wRKuumt42UwUg6Ogjy6LL-c'
+    bot_token = '5289637475:AAFdCLoE7ywnesB31CJLZAafyrb9otgktQM'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
